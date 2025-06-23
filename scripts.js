02559d8cf2a97daba8e45c4edab8cb56b7161984
jaquiel <jaquieljorge@hotmail.com>
2025-06-22 21:10:09 -0300

    Projeto Starbucks first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/*
+   declara uma variável 
+   let nota1 = 9.5
+   var cidade = "Sorocaba"
+   const primeiroCliente = "Jaquiel "
+*/
+
+
+
+
+let circulo = document.querySelector(".circulo")
+
+function trocarCor(cor){
+ 
+   circulo.style.background = cor;
+
+}
+function trocarImagem(endereco){
+    imagem.src = endereco
+}  
